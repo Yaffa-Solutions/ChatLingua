8a04c0ad7574f28f794c6417574cd7af5f6baa3d
AyshaVolidis <ayshavolidis@gmail.com>
2025-08-17 13:34:02 +0300

    Refactor configuration files and database connection; add environment variable support and improve structure

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
+const { database } = require('../config');
 
-const { app } = require('../config/index');
 
 const pool= new Pool({
-  connectionString: app.database,
+  connectionString: database.databaseUrl,
 })
 
 module.exports = pool;
