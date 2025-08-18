de11bc1f1671068c0d742fac46ce39e62599131a
Nada.Salha <128420328+NadaSalha@users.noreply.github.com>
2025-08-18 17:37:05 +0300

    test for Signup , login

---
+++ src/test/user.test.js
@@ -0,0 +1,73 @@
+const app = require('../../app');
+const request = require('supertest');
+
+describe('Auth Endpoints', () => {
+  test('SIGNUP user',  () => {
+  return  request(app)
+      .post('/register')
+      .send({ username: 'nada', password: 'pass123' })
+      .expect('Content-Type', /json/)
+      .then((res)=>{
+          expect(res.status).toBe(201);
+          expect(res.body).toHaveProperty('message', 'user registered successfully ');
+          expect(res.body).toHaveProperty('token');
+          expect(typeof res.body.token).toBe('string');
+         expect(res.body).toHaveProperty('data');
+         expect(res.body.data).toHaveProperty('username', 'nada');
+      })
+     });
+
+
+     test('SIGNUP USER DUPLICATE', () => {
+
+      return request(app)
+      .post('/register')
+      .send({ username: 'nada', password: 'pass123' })
+      .expect('Content-Type',/json/)
+      .then((res)=>{
+          expect(res.status).toBe(500);         
+          expect(res.body).toHaveProperty('message', 'duplicate key value violates unique constraint \"users_username_key\"');        
+      })
+     })
+     
+    
+  test('LOGIN user', async () => {
+    return request(app)
+      .post('/login')
+      .send({ username: 'nada', password: 'pass123' })
+      .expect('Content-Type', /json/)
+    .then((res)=>{
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('message', 'user logged in successfully ');
+    expect(res.body).toHaveProperty('token');
+    expect(typeof res.body.token).toBe('string');
+    })
+   
+  });
+
+
+
+  test('LOGIN WITH WRONG PASSWORD FAIL', () => {
+    return request(app)
+    .post('/login')
+    .send({username:'nada',password:'wrongPassword'})
+    .expect('Content-Type',/json/)
+    .then((res)=>{
+      expect(res.status).toBe(401);
+      expect(res.body).toHaveProperty('message');
+      expect(res.body.success).toBe(false);
+    })
+  })
+  
+  test('LOGIN with non-existing user should fail', () => {
+    return request(app)
+      .post('/login')
+      .send({ username: 'nadasalha', password: 'pass123' })
+      .expect('Content-Type', /json/)
+      .then((res) => {
+        expect(res.status).toBe(404);
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.success).toBe(false);
+      });
+  });
+});
